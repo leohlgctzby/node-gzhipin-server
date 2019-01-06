e0889c7de8d2b41c0078d64ffdfc48187ee15309
leohlgctzby <18207221@qq.com>
2019-01-06 17:01:42 +0800

    后台简单编码并测试postman

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//用户注册的路由
+router.post('/register', function(rep, res){
+  const {username, password} = rep.body
+  if(username=='admin'){
+    res.send({code: 1, msg: '此用户已存在'})
+  } else {
+    res.send({code: 0, data: {_id: 'abc', username, password}})
+  }
+})
+
 module.exports = router;
